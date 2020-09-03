e8bce76856a96fce19eb06cbd01d6beb76a327a6
tyqiangz <tay.yongqiang@gmail.com>
2020-09-03 10:46:53 +0800

    minor changes

--- React - The Complete Guide/base-syntax--01-props-custom-cmp/src/App.js
+++ React - The Complete Guide/base-syntax--01-props-custom-cmp/src/App.js
@@ -3,15 +3,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Max", age: 28},
+      {name: "Manu", age: 29},
+      {name: "Stephanie", age: 26}
+    ]
+  }
+
+  switchNameHandler = () => {
+    //console.log("Was clicked!");
+    this.setState( {
+      persons: [
+        {name: "Alpha", age: 38},
+        {name: "Beta", age: 39},
+        {name: "Carol", age: 36}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
        <h1>Hi, I'm a React App</h1>
        <p>This is really working!</p>
-       <button>Switch name</button>
-       <Person name="Max" age="28" />
-       <Person name="Manu" age="29" >My Hobbies: Racing</Person>
-       <Person name="Stephanie" age="26" />
+       <button onClick={this.switchNameHandler}>Switch name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
