a072c371bfd1befa6b9aec8c0f195ca308fab534
tyqiangz <tay.yongqiang@gmail.com>
2020-09-04 21:18:22 +0800

    renamed assignment 2

--- React - The Complete Guide/lists-conditionals--assignment-problem/src/App.js
+++ React - The Complete Guide/lists-conditionals--assignment-problem/src/App.js
@@ -1,31 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import Validation from './Validation/Validation';
-import Char from './Char/Char';
 
 class App extends Component {
-  state = {
-    userInput: ''
-  }
-
-  inputChangedHandler = ( event ) => {
-    this.setState( { userInput: event.target.value } );
-  }
-
-  deleteCharHandler = ( index ) => {
-    const text = this.state.userInput.split('');
-    text.splice(index, 1);
-    const updatedText = text.join('');
-    this.setState({userInput: updatedText});
-  }
 
   render () {
-    const charList = this.state.userInput.split('').map((ch, index) => {
-      return <Char 
-        character={ch} 
-        key={index}
-        clicked={() => this.deleteCharHandler(index)} />;
-    });
 
     return (
       <div className="App">
@@ -39,13 +17,6 @@ class App extends Component {
         </ol>
         <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
         <hr />
-        <input
-          type="text"
-          onChange={this.inputChangedHandler}
-          value={this.state.userInput} />
-        <p>{this.state.userInput}</p>
-        <Validation inputLength={this.state.userInput.length} />
-        {charList}
       </div>
     );
   }
