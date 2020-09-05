c83a0190c4860a3b24661fa72dbd27268e00fcfc
tyqiangz <tay.yongqiang@gmail.com>
2020-09-05 22:50:39 +0800

    added tutorial

--- React - The Complete Guide/lists-conditionals--lists-finished/src/App.js
+++ React - The Complete Guide/lists-conditionals--lists-finished/src/App.js
@@ -46,7 +46,8 @@ class App extends Component {
 
   render () {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
